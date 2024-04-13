dfe0aad7879f59cd2309cf55bfaeb9f65155e843
grpse <gilbertoribeiropazdarosa@gmail.com>
2024-04-13 17:14:05 -0300

    adding testing for auth

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -4,47 +4,68 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { UsersService } from '@/users/users.service';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from '@/prisma.service';
+import { jwtConstants } from './constants';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('auth > AuthController >', () => {
   let controller: AuthController;
   let authService: AuthService;
+  let localStrategy: LocalStrategy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
         AuthService,
-        JwtService,
         UsersService,
         LocalStrategy,
         JwtStrategy,
         PrismaService,
       ],
+      imports: [
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: { expiresIn: '60s' },
+        }),
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
     authService = module.get<AuthService>(AuthService);
+    localStrategy = module.get<LocalStrategy>(LocalStrategy);
   });
 
-  describe('should signup and account and login', () => {
-    const signupDto = { username: 'test', password: '123' };
-    const loginDto = { username: 'test', password: '123' };
-    const authedUserDto = { username: 'test', accessToken: '123' };
+  it('should signup and account and login', async () => {
+    const username = Math.random().toString(36).substring(7);
+    const password = Math.random().toString(36).substring(7);
 
-    it('should signup', async () => {
-      const result = await controller.signup(signupDto);
-      expect(result).toEqual(expect.objectContaining(authedUserDto));
-    });
+    const signup = await controller.signup({ username, password });
+    const user = await authService.validateUser(username, password);
+    const login = await controller.login(user);
 
-    it('should login', async () => {
-      const user = await authService.validateUser(
-        loginDto.username,
-        loginDto.password,
-      );
-      const result = await controller.login(user);
-      expect(result).toEqual(expect.objectContaining(authedUserDto));
-    });
+    expect(signup).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        username,
+      }),
+    );
+    expect(login).toEqual(
+      expect.objectContaining({
+        accessToken: expect.any(String),
+      }),
+    );
+  });
+
+  it('should fail to login with invalid password', async () => {
+    const username = Math.random().toString(36).substring(7);
+    const password = Math.random().toString(36).substring(7);
+
+    await controller.signup({ username, password });
+
+    await expect(
+      localStrategy.validate(username, 'invalid password'),
+    ).rejects.toThrow(UnauthorizedException);
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
   async login(user: AuthedUserDto) {
     const payload = { username: user.username, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken: this.jwtService.sign(payload),
     };
   }
 
