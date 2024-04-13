00332d26e82a9002329ea5138e98d9e16f91d2dd
grpse <gilbertoribeiropazdarosa@gmail.com>
2024-04-13 17:31:01 -0300

    tests for auth

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,35 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
-import { UsersService } from '@/users/users.service';
-import { JwtModule } from '@nestjs/jwt';
-import { PrismaService } from '@/prisma.service';
-import { jwtConstants } from './constants';
 import { UnauthorizedException } from '@nestjs/common';
+import { randomString } from 'test/utils/randomString';
+import { AuthModule } from './auth.module';
 
-describe('auth > AuthController >', () => {
+describe('auth > AuthController > integration', () => {
   let controller: AuthController;
   let authService: AuthService;
   let localStrategy: LocalStrategy;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [
-        AuthService,
-        UsersService,
-        LocalStrategy,
-        JwtStrategy,
-        PrismaService,
-      ],
-      imports: [
-        JwtModule.register({
-          secret: jwtConstants.secret,
-          signOptions: { expiresIn: '60s' },
-        }),
-      ],
+      imports: [AuthModule],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -38,8 +22,8 @@ describe('auth > AuthController >', () => {
   });
 
   it('should signup and account and login', async () => {
-    const username = Math.random().toString(36).substring(7);
-    const password = Math.random().toString(36).substring(7);
+    const username = randomString(7);
+    const password = randomString(7);
 
     const signup = await controller.signup({ username, password });
     const user = await authService.validateUser(username, password);
@@ -59,8 +43,8 @@ describe('auth > AuthController >', () => {
   });
 
   it('should fail to login with invalid password', async () => {
-    const username = Math.random().toString(36).substring(7);
-    const password = Math.random().toString(36).substring(7);
+    const username = randomString(7);
+    const password = randomString(7);
 
     await controller.signup({ username, password });
 
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AuthModule } from './auth.module';
+import { randomString } from 'test/utils/randomString';
+import { AuthedUserDto } from './dto/authed-user.dto';
 
-describe('AuthService', () => {
-  let service: AuthService;
-
-  beforeEach(async () => {
+describe('auth > AuthService > integration', () => {
+  const createNewUser = async (): Promise<{
+    createdUser: AuthedUserDto;
+    username: string;
+    password: string;
+    service: AuthService;
+  }> => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [AuthModule],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    const service = module.get<AuthService>(AuthService);
+    const username = randomString(7);
+    const password = randomString(7);
+    const createdUser = await service.createUser(username, password);
+
+    return {
+      createdUser,
+      username,
+      password,
+      service,
+    };
+  };
+
+  it('should signup a user', async () => {
+    const { createdUser, username } = await createNewUser();
+    expect(createdUser.username).toEqual(username);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should validate a user', async () => {
+    const { username, password, service } = await createNewUser();
+    const res = await service.validateUser(username, password);
+    expect(res.username).toEqual(username);
+  });
+
+  it('should validate a user with invalid password', async () => {
+    const { username, service } = await createNewUser();
+    const res = await service.validateUser(username, 'invalidPassword');
+    expect(res).toBeNull();
+  });
+
+  it('should login a user', async () => {
+    const { createdUser, service } = await createNewUser();
+    const res = await service.login(createdUser);
+    expect(res.accessToken).toBeDefined();
   });
 });
